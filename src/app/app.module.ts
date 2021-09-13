ff3d032bda33e97115729441923ef781a5836319
klastill <gyb0501@gmail.com>
2021-09-13 18:05:28 +0900

    visualize view encapsulate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ByeComponent } from './bye/bye.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    ByeComponent
   ],
   imports: [
     BrowserModule,
