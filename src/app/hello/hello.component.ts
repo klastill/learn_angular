c86f05c32e8847162327d450e3cb13fccd0d2579
klastill <gyb0501@gmail.com>
2021-09-13 11:40:02 +0900

    *ngFor

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Item {
+  index: number,
+  value: string
+}
+
 @Component({
   selector: 'hello',
   templateUrl: './hello.component.html',
@@ -8,6 +13,24 @@ import { Component, OnInit } from '@angular/core';
 export class HelloComponent implements OnInit {
   name: string = 'unknown';
   isShow: boolean = true;
+  items: Item[] = [
+    {
+      index: 1,
+      value: "First"
+    },
+    {
+      index: 2,
+      value: "Second"
+    },
+    {
+      index: 3,
+      value: "Third"
+    },
+    {
+      index: 4,
+      value: "Fourth"
+    }
+  ];
 
   setName(name: string) {
     this.name = name;
